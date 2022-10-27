f30139b628e6eb6906a503590ac664d168659c74
PatoVerde <leandrodenos@gmail.com>
2022-10-27 14:05:32 -0300

    First commit

---
+++ contracts/ERC20Standar.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+
+
+contract ERC20Standar {
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const ALCHEMY_API_KEY = "KEY";
+const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: "F827CH3MPVM3XTU91M4YJHBKTUC94SWKXX", // Your Etherscan API key
+  },
+
+};
