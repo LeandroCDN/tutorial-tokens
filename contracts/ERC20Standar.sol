5ed6ea1f35ebaeba7b78ab3ef44655439a687f99
PatoVerde <leandrodenos@gmail.com>
2022-10-27 14:38:34 -0300

    standar token erc20

--- contracts/ERC20Standar.sol
+++ contracts/ERC20Standar.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract ERC20Standar {
-
+contract ERC20Standar is ERC20  {
+  constructor(string memory name, string memory token, uint initialmint) ERC20(name,token){
+    _mint(msg.sender, initialmint * 1 ether);
+  }
 }
